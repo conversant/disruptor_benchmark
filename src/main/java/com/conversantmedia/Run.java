39162d63156c280a4e278b8643ae5eee92539b34
John Cairns <jcairns@conversantmedia.com>
2016-02-29 15:29:11 -0600

    Conversant Disruptor 1.2.4

--- src/main/java/com/conversantmedia/Run.java
+++ src/main/java/com/conversantmedia/Run.java
@@ -5,9 +5,9 @@ package com.conversantmedia;
  */
 public interface Run {
     int NTHREAD = Math.min(Runtime.getRuntime().availableProcessors(), 4);
-    int FORK = 3;
+    int FORK = 2;
     int WARM_ITER = 3;
-    int MEAS_ITER = 6;
+    int MEAS_ITER = 8;
     int QUEUE_SIZE = 1024;
     int OFFER_COUNT = 1000000;
     Long LONGVAL = 0xdeadbeefL;
