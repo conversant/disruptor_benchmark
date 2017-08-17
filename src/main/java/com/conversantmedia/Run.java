87727ab6057aaa90030f2b7dd8301096a0f2600d
John Cairns <jcairns@conversantmedia.com>
2017-08-17 13:28:49 -0500

    1.2.11

--- src/main/java/com/conversantmedia/Run.java
+++ src/main/java/com/conversantmedia/Run.java
@@ -7,9 +7,9 @@ public final class Run {
     static final int MULTITHREAD = 4*Runtime.getRuntime().availableProcessors();
     static final int NTHREAD = Math.min(Runtime.getRuntime().availableProcessors(), 4);
     static final int FORK = 1;
-    static final int WARM_ITER = 10;
-    static final int MEAS_ITER = 20;
-    static final int QUEUE_SIZE = 2048;
+    static final int WARM_ITER = 8;
+    static final int MEAS_ITER = 15;
+    static final int QUEUE_SIZE = 1024;
     static final int OFFER_COUNT = 1_000_000;
     static final Long LONGVAL = 0xba53ba1L;
 
